f69532b0d3829012e43a87d3739683867d0e9369
Christian A Millard <cmillar70@ccs.local>
2019-11-18 10:22:17 -0500

    adding newping and hello functions

---
+++ newping/newping.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+#include <NewPing.h>
+
+Servo myservo;
+#define TRIGGER_PIN 8
+#define ECHO_PIN 6
+#define MAX_DISTANCE 200
+
+int cm = 0;
+NewPing myHCSR04(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+ Serial.begin(9600);
+ myservo.attach(9);
+} 
+
+void loop() {
+    cm = myHCSR04.ping_cm();
+     Serial.println(cm);
+     delay(50); 
+    if ( cm != 0 && cm < 150)
+  {
+    if (cm < 10) {
+      myservo.write(100);
+    }
+    if (cm > 10) {
+       myservo.write(80);
+    }
+  }
+}
+
